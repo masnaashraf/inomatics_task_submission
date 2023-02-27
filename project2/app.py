a58167cc95b0c764c0c1c4ee46654f80f260554d
MASNA <64076575+masnaashraf@users.noreply.github.com>
2023-02-27 11:21:55 +0400

    Add files via upload

---
+++ project2/app.py
@@ -0,0 +1,17 @@
+
+import streamlit as st
+
+
+st.title("MASNA")
+
+st.header("Data Science intern@Innomatics Reserach lab")
+
+
+st.subheader("Passionate about data and in a journey to drill deep into data science and would love to connect people interested in data:smile::smile:")
+
+st.header("Connect with me")
+
+if st.button("linkedin"):
+    st.write("https://www.linkedin.com/in/masna-ashraf-4952461b7/")
+if st.button("github"):
+    st.write("https://github.com/masnaashraf")
